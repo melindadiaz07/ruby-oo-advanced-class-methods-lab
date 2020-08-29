3dbf7fc27564a8f47c667d7efae468a25ce61da2
Melindadiaz07 <melindadiaz@me.com>
2020-08-29 14:51:17 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +8,64 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+     self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ?
+    self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name }
+  end
+
+  def self.new_from_filename(file_data)
+    file_array = file_data.split(/[-.]/)
+    artist = file_array[0].strip
+    song_name = file_array[1].strip
+    song = self.new_by_name(song_name)
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(file_data)
+    file_array = file_data.split(/[-.]/)
+    artist = file_array[0].strip
+    song_name = file_array[1].strip
+    song = self.create_by_name(song_name)
+    song.artist_name = artist
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
   def save
     self.class.all << self
   end
 
 end
+
+song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
